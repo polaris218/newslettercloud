3a71704aa8ecd013c19384a789d88a41855dd98a
actiongeek <software.dev0218@gmail.com>
2020-02-25 14:13:29 +0900

    images

--- git-fake-commit.bat
+++
@@ -1,3 +0,0 @@
-git filter-branch -f --env-filter "GIT_AUTHOR_NAME='actiongeek'; GIT_AUTHOR_EMAIL='software.dev0218@gmail.com'; GIT_COMMITTER_NAME='actiongeek'; GIT_COMMITTER_EMAIL='software.dev0218@gmail.com';"
-
-git push --force origin master
